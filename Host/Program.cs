b595b8d92c96846dcf0d7525270befdf06021f97
gilli-s <36599771+gilli-s@users.noreply.github.com>
2019-12-13 00:51:08 +0500

    world

--- Host/Program.cs
+++ Host/Program.cs
@@ -10,7 +10,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World");
         }
     }
 }
